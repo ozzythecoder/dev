12183e083efc0004525fa6da127886765d8e3f85
August McAllister <august.chm@gmail.com>
2023-04-05 16:51:08 -0500

    Added spotlight prop to body text component - will adjust margins differently

--- src/lib/components/BodyText.svelte
+++ src/lib/components/BodyText.svelte
@@ -1,8 +1,17 @@
-<p class="body-text">
+<script>
+  export let spotlight = false;
+</script>
+
+<p class={`body-text ${spotlight ? "spotlight-description" : ""}`}>
 	<slot />
 </p>
 
 <style>
+  .spotlight-description {
+    /* outline: 1px solid white; */
+    margin: 0.4rem 0 !important;
+  }
+
   p :global(.center) {
     text-align: center;
     margin: 0.4rem 0;
