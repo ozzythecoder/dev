cdb6a9a6c1dfa2c750973d146f69aea5392f8dc7
August McAllister <august.chm@gmail.com>
2023-08-30 11:20:57 -0500

    Added placeholder blog content; began styling

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,12 +7,25 @@
 <h1>Hey</h1>
 <div>
   {#each postData as post}
-    <p>{post.metadata?.title}</p>
+    <a href={post.path} class="link unstyled" >
+
+    <div class="post-link">
+      <h2>{post.metadata?.title}</h2>
+      <p>{post.metadata?.description}</p>
+    </div>
+    </a>
   {/each}
 </div>
 
 <style>
   h1 {
     font-size: 100px;
+  }
+
+  .post-link {
+    border: 1px solid gray;
+    border-radius: 10px;
+    margin: 5px;
+    padding: 20px;
   }
 </style>
