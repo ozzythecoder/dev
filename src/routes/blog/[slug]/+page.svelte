66a1d6098d2ec910cc9d6a17bef3c0816d063c06
August McAllister <august.chm@gmail.com>
2023-09-19 14:18:36 -0500

    Styles now apply properly to rendered markdown.

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -24,8 +24,11 @@
   @use "../../../lib/styles/font-mixins";
 
   main {
-    margin: 4rem 15%;
-    max-width: 1000px;
+    margin: 4rem 7%;
+    max-width: 760px;
+    @media screen and (min-width: 760px) {
+      margin: 4rem auto;
+    }
   }
 
   nav {
@@ -48,12 +51,48 @@
 
   .muted-date-text {
     color: text-colors.$text-muted;
+    padding-top: 3px;
     font-style: italic;
   }
 
   article {
     @media screen and (min-width: 760px) {
       padding-top: 0.3rem;
+    }
+
+    :global(p) {
+      margin: 0.7rem 0;
+    }
+
+    :global(h1) {
+      @include font-mixins.font-size(font-mixins.$title-font-sizes);
+    }
+    :global(h2) {
+      @include font-mixins.font-size(font-mixins.$h2-sizes);
+    }
+
+    :global(h3) {
+      text-align: left;
+      margin: 1.3rem 0;
+    }
+
+    :global(pre) {
+      padding: 1rem 1.5rem;
+      background: rgb(37,37,37);
+      background: linear-gradient(133deg, rgba(37,37,37,1) 0%, rgba(27,27,27,1) 50%, rgba(0,0,0,1) 100%);
+      overflow-x: scroll;
+      font-size: 14px;
+
+      @media screen and (min-width: 760px) { 
+        margin: 2rem 6rem;
+        font-size: 14px;
+      }
+    }
+
+    :global(hr) {
+      max-width: 90%;
+      margin: 1.3rem auto;
+      border-color: text-colors.$text-muted; 
     }
   }
 
@@ -62,46 +101,8 @@
     margin: 1.3rem auto 0.3rem;
 
     @media screen and (min-width: 760px) {
-      margin: 2.3rem auto 1.3rem;
+      margin: 1.7rem auto 0.5rem;
     }
   }
 
-  :global(h1) {
-    @include font-mixins.font-size(font-mixins.$title-font-sizes);
-  }
-
-  :global(h2) {
-    @include font-mixins.font-size(font-mixins.$h2-sizes);
-  }
-
-  :global(h3) {
-    margin: 1.3rem 0;
-  }
-
-  :global(pre) {
-    padding: 1rem 1.5rem;
-    background: rgb(37,37,37);
-    background: linear-gradient(133deg, rgba(37,37,37,1) 0%, rgba(27,27,27,1) 50%, rgba(0,0,0,1) 100%);
-    overflow-x: scroll;
-    font-size: 14px;
-
-    @media screen and (min-width: 760px) { 
-      margin: 2rem 6rem;
-      font-size: 14px;
-    }
-  }
-
-  :global(p) {
-    margin: 0.7rem 0 !important;
-  }
-
-  :global(hr) {
-    max-width: 90%;
-    margin: 1.3rem auto;
-    border-color: text-colors.$text-muted; 
-  }
-
-  :global(p, ul, li, code) {
-    @include font-mixins.font-size(font-mixins.$p-sizes);
-  }
 </style>
